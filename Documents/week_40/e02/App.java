4d34ecb7202d260fc3186c6f63997e91b4276ed6
Oliver Chandler <choliver2005@gmail.com>
2024-10-03 16:02:13 +0300

    changes made

---
+++ Documents/week_40/e02/App.java
@@ -0,0 +1,15 @@
+package week_40.e02;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        String name = scanner.nextLine();
+        System.out.println("You entered: " + name);
+        for(int i =0; i < name.length(); i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
